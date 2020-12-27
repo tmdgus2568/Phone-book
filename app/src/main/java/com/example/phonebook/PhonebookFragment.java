af1734a2ef1ae5480d869793e31971f711f0f39f
seunghyunAnt <seunghyun4525@gmail.com>
2020-12-27 17:36:48 +0900

    feat: Match listview size

--- app/src/main/java/com/example/phonebook/PhonebookFragment.java
+++ app/src/main/java/com/example/phonebook/PhonebookFragment.java
@@ -13,6 +13,8 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class PhonebookFragment extends Fragment {
     ListView mListView;
     @Nullable
@@ -24,8 +26,11 @@ public class PhonebookFragment extends Fragment {
         ListAdapter adapter = new ListAdapter();
 
         // 아이템 넣는 부분
-        adapter.addItem("승현", "010-9422-5397");
-        adapter.addItem("승현22", "010-1234-5678");
+        String names[] = {"승현001", "승현002", "승현003", "승현004", "승현005", "승현006", "승현007", "승현008"};
+        String phones[] = {"010-1111-1111", "010-2222-2222", "010-3333-3333", "010-4444-4444", "010-5555-5555", "010-6666-6666", "010-7777-7777", "010-8888-8888"};
+        for(int i = 0; i < 8; i++) {
+            adapter.addItem(names[i], phones[i]);
+        }
 
         mListView.setAdapter(adapter);
         return v;
