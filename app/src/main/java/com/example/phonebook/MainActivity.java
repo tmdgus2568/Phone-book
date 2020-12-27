e4a8db5fb3b6d442af69426a9f520c7814615ca2
hsh <tmdgus2568@gmail.com>
2020-12-27 16:08:42 +0900

    bottom navigation

--- app/src/main/java/com/example/phonebook/MainActivity.java
+++ app/src/main/java/com/example/phonebook/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.phonebook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.navigation);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, new CallFragment()).commit();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_call:
+                        getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, new CallFragment()).commit();
+                        break;
+                    case R.id.action_phonebook:
+                        getSupportFragmentManager().beginTransaction().add(R.id.frameLayout, new PhonebookFragment()).commit();
+                        break;
+                }
+                return true;
+            }
+        });
     }
+
+
 }
